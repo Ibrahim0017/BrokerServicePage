cd71de4db8e2c9f82aff3c75937c85d83792aaef
Ibrahim0017 <ibrahim72336@gmail.com>
2024-10-18 20:12:28 +0100

    first commit

---
+++ brokers.js
@@ -0,0 +1,12 @@
+function toggleDetails(event) {
+    let content = document.getElementById("extraContent");
+   
+  
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      
+    } else {
+      content.style.display = "block";
+    }
+  }
+
